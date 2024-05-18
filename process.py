af98706e438438c806280f00cb6ad57517a11159
shashidharbabu <shashidharbabu.p19@gmail.com>
2024-05-18 20:00:45 +0530

    Text generation using Gemini.

---
+++ process.py
@@ -0,0 +1,16 @@
+import google.generativeai as genai
+import os
+genai.configure(api_key = os.getenv("GOOGLE_API_KEY"))
+generation_config = {
+    
+    "temperature": 0.9,
+    "top_k": 1,
+    "top_p": 1,
+    "max_output_tokens":2048
+    
+}
+model = genai.GenerativeModel("gemini-pro", generation_config = generation_config)
+response = model.generate_content(["Create a study schedule for the whole day."])
+
+print(response.text)
+
